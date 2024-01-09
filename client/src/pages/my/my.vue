6544fe11771e72ebc901fc5bdea48d6bfad567d6
swanine <haohao1500@foxmail.com>
2024-01-09 21:32:22 +0800

    feature: add my page

---
+++ client/src/pages/my/my.vue
@@ -0,0 +1,25 @@
+<template>
+  <view> count: {{ count }} </view>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import Taro from "@tarojs/taro";
+
+export default {
+  setup() {
+    const count = ref(9)
+
+    onMounted(() => {
+
+    });
+
+    return {
+      count
+    };
+  },
+};
+</script>
+
+<style>
+</style>
