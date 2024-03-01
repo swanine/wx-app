e2c39ea5e96e314b18f4b2d183b39480e5c692c9
swanine <haohao1500@foxmail.com>
2024-03-01 18:18:34 +0800

    feature: 工作日报接口

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,6 @@
 import request from '../utils'
 // import request from '../utils/request/cloud'
 
-
-
 // 获取评审列表
 export function getEvaluateList(data) {
   return request.post({ url: '/wx/evaluate/getEvaluateList', data })
@@ -36,3 +34,39 @@ export function getEvaluateDetail(id) {
 // export function getEvaluateDetail_C(id) {
 //   return request.post(`/wx/evaluate/getEvaluateDetail?id=${id}`)
 // }
+
+// 工作日报相关
+
+// 工作日报列表
+export function getDailyReportList(data) {
+  return request.post({ url: '/wx/daily/getDailyReportList', data })
+}
+
+// 新增或更新
+export function addOrUpdateReport(data) {
+  return request.post({
+    url: '/wx/daily/addOrUpdateReport',
+    data
+  })
+}
+
+// 工作日报详情
+export function getDailyReportDetail(id) {
+  return request.post({
+    url: `/wx/daily/getDailyReportDetail?id=${id}`,
+  })
+}
+
+// 登录
+export function wxLogin(code) {
+  return request.post({
+    url: `/wx/login?code=${code}`,
+  })
+}
+
+// 手机号
+export function getUserPhone(code) {
+  return request.post({
+    url: `/wx/getUserPhone?code=${code}`,
+  })
+}
