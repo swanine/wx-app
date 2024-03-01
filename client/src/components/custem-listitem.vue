202196ae06928fba4a874bc5fd0e86e0efe364bf
swanine <haohao1500@foxmail.com>
2024-03-01 18:19:13 +0800

    feature: 列表组件新增类型

--- client/src/components/custem-listitem.vue
+++ client/src/components/custem-listitem.vue
@@ -1,23 +1,34 @@
 <template>
   <view>
     <view style="font-size: 12px; color: #999999">{{ itemData.scNo }}</view>
-    <view style="font-size: 16px;font-weight: 600; color: #333; margin-bottom: 4px">{{
-      itemData.projectName
-    }}</view>
-    <nut-tag color="#E9E9E9" textColor="#999999" v-if="itemData.state === 1"
-      >待评价</nut-tag
+    <view
+      style="font-size: 16px; font-weight: 600; color: #333; margin-bottom: 4px"
+      >{{ itemData.projectName }}</view
     >
-    <nut-tag type="primary" v-if="itemData.state === 2">完成评价</nut-tag>
-    <nut-tag
-      type="success"
-      v-if="itemData.state === 2 && itemData.serviceScore > 4"
-      >好评</nut-tag
-    >
-    <nut-tag
-      type="danger"
-      v-if="itemData.state === 2 && itemData.serviceScore < 2"
-      >差评</nut-tag
-    >
+    <template v-if="tag === 'work'">
+      <nut-tag type="success" v-if="itemData.status === 1">进行中</nut-tag>
+      <nut-tag color="#5389f0" textColor="#ffffff" v-if="itemData.status === 2"
+        >已结束</nut-tag
+      >
+    </template>
+
+    <template v-else>
+      <nut-tag color="#E9E9E9" textColor="#999999" v-if="itemData.state === 1"
+        >待评价</nut-tag
+      >
+      <nut-tag color="#5389f0" v-if="itemData.state === 2">完成评价</nut-tag>
+      <nut-tag
+        type="success"
+        v-if="itemData.state === 2 && itemData.serviceScore > 4"
+        >好评</nut-tag
+      >
+      <nut-tag
+        color="#FA685D"
+        v-if="itemData.state === 2 && itemData.serviceScore < 2"
+        >差评</nut-tag
+      >
+    </template>
+
     <view style="font-size: 12px; color: #999999">{{
       dayjs(itemData.commitTime).format("YYYY-MM-DD HH:mm:ss")
     }}</view>
@@ -37,6 +48,11 @@ export default {
         state: 1,
         commitTime: "2023-12-14",
       }),
+    },
+
+    tag: {
+      type: String,
+      default: "work",
     },
   },
   setup() {
