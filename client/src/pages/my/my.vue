3e983d5c765cc28e1b2cbd6849a86db34cd7da77
swanine <haohao1500@foxmail.com>
2024-03-21 21:19:46 +0800

    feature: 我的页面

--- client/src/pages/my/my.vue
+++ client/src/pages/my/my.vue
@@ -1,21 +1,47 @@
 <template>
-  <view> count: {{ count }} </view>
+  <view> count: {{ count }} 
+    <button open-type="getPhoneNumber" @getphonenumber="callback">按钮</button>
+    <!-- <button @click="login">按钮</button> -->
+  </view>
 </template>
 
 <script>
 import { onMounted, ref } from "vue";
 import Taro from "@tarojs/taro";
+import { wxLogin, getUserPhone } from "../../api";
 
 export default {
   setup() {
     const count = ref(9)
 
     onMounted(() => {
-
+      
     });
 
+    // const login = () => {
+    //   Taro.login({
+    //     success: function (res) {
+    //       console.log(res);
+    //       wxLogin(res.code).then(res => {
+    //         console.log(res);
+    //       })
+    //     }
+    //   })
+    // }
+
+    const callback = (e) => {
+      console.log(e);
+      if (e.detail) {
+        getUserPhone(e.detail.code).then(res => {
+          console.log(res);
+        })
+      }
+    }
+
     return {
-      count
+      count,
+      callback,
+      // login
     };
   },
 };
